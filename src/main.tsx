93987e9eaae8ee0360b31c7bf23fe6ccef5bbc8a
Deepak Chapagain <dchapagain.dev@gmail.com>
2026-01-26 22:25:22 -0600

    [AA-1101] - config a11y rules and checks

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,15 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@/App.tsx'
 
+if (import.meta.env.DEV) {
+  void (async () => {
+    const axe = (await import('@axe-core/react')).default
+    const React = await import('react')
+    const ReactDOM = await import('react-dom')
+    axe(React, ReactDOM, 1000)
+  })()
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
